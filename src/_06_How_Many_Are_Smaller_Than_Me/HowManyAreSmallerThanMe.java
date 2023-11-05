2dfb4267749f62123c996b07dc492813f1e5f11a
opie10 <augie@keatinge.com>
2023-11-05 11:40:59 -0800

    Update HowManyAreSmallerThanMe.java

--- src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
+++ src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
@@ -5,62 +5,60 @@ import _05_Intro_to_AVL_Trees.AVLTree;
 
 public class HowManyAreSmallerThanMe {
 
-    /*
-     * Given an AVLTree filled with the random numbers from 1 to 20, and a
-     * single number within the AVL Tree, figure out how many numbers in the
-     * AVLTree are smaller than the provided number and return the result.
-     * 
-     * You may want to create a helper method to search through the AVLTree and
-     * count iteratively or recursively.
-     */
+	/*
+	 * Given an AVLTree filled with the random numbers from 1 to 20, and a single
+	 * number within the AVL Tree, figure out how many numbers in the AVLTree are
+	 * smaller than the provided number and return the result.
+	 * 
+	 * You may want to create a helper method to search through the AVLTree and
+	 * count iteratively or recursively.
+	 */
 
-    public int howManyAreSmallerThanMe(AVLTree<Integer> avlTree, int me) {
-    	System.out.println(me);
-    	
-  int trueCount= sAd(avlTree.getRoot(),0,me);
-        return trueCount;
+	public int howManyAreSmallerThanMe(AVLTree<Integer> avlTree, int me) {
+		System.out.println(me);
 
-    }
-    public int sAd(AVLNode<Integer>avl,int counter, int me) {
-    	if (avl!=null) {
-			if (me>avl.getValue()) {
-				
-				int c =count(avl.getLeft(),0);
-				counter =counter+ c;
-				
-				counter = sAd(avl.getRight(),counter, me);
-				
-			}
-			if (me<=avl.getValue()) {
-				
-					counter = sAd(avl.getLeft(),counter, me);
-				
-				
-			}
-		}
-		return counter;
-    	
-    }
-    public int count(AVLNode<Integer>avl,int counter) {
-	if (counter==0) {
-		counter =1;
+		int trueCount = sAd(avlTree.getRoot(), 0, me);
+		return trueCount;
+
 	}
-    	if (avl!=null) {
-			
-		
-    	if (avl.getLeft()!=null) {
-    		counter = counter+1;
-		
-    	counter =count(avl.getLeft(),counter);
-    	}
 
-    	if (avl.getRight()!=null) {
-    		counter = counter+1;
-    	counter =count(avl.getRight(),counter);
-    	}
-    	}
-    	return counter;
-    }
-    
+	public int sAd(AVLNode<Integer> avl, int counter, int me) {
+	    if (avl != null) {
+	        if (me > avl.getValue()) {
+	            counter++; 
+	            
+	            counter = sAd(avl.getLeft(), counter, me);
+	            counter = sAd(avl.getRight(), counter, me);
+	            
+	            
+	        } else {
+	            counter = sAd(avl.getLeft(), counter, me);
+	        }
+	    }
+	    
+	    return counter;
+	}
+
+	public int count(AVLNode<Integer> avl, int counter) {
+		
+	    if (counter == 0) {
+	        counter = 1;
+	    }
+	    if (avl != null) {
+	        if (avl.getLeft() != null) {
+	        	
+	        	
+	            counter = count(avl.getLeft(), counter);
+	        }
+
+	        if (avl.getRight() != null) {
+	        	
+	            counter = count(avl.getRight(), counter);
+
+  }
+	    }
+	    
+	    return counter;
+	}
 
 }
